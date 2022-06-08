0556bb9ec799faa740abdc713ed6e3138d4d85cf
Alex Linden <alextl2004@gmail.com>
2022-06-07 17:02:37 -0700

    added special word finder and fixed changes from review

--- wordfinder.py
+++ wordfinder.py
@@ -1,20 +1,33 @@
 from random import choice as pick_word
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self,file_path):
+        """creates a list of words from a file path and a prints number of words in the list"""
         self.file_path = file_path
-        self.words_list = self.populate_words_list()
-        self.message = print( f"{len(self.words_list)} words read")
+        self.words_list = self.get_words_list()
+        print( f"{len(self.words_list)} words read")
 
     def random(self):
+        """selects a random word from the word list in the instance"""
         return pick_word(self.words_list)
 
-    def populate_words_list(self):
+    def get_words_list(self):
+        """called when the instance is created to get words_list from file"""
         file = open(self.file_path)
-        word_list = []
-        for line in file:
+        return [line.strip() for line in file]
 
-            word_list.append(line[:-1])
-        return word_list
+
+class SpecialWordFinder(WordFinder):
+    """This sub class accounts for lists that don't start with a letter and doesn't include them"""
+
+    def __init__(self, file_path):
+        """creates a list of words from a file path and a prints number of words in the list"""
+        super().__init__(file_path)
+
+    def get_words_list(self):
+        """called when the instance is created to get words_list from file"""
+        file = open(self.file_path)
+        return [line.strip() for line in file if line[0].lower() in LETTERS ]
